8a800bef9c4901756911dd7fd413074bb2f65f72
Fran Díaz <trianafran@gmail.com>
2024-06-05 21:21:31 +0200

    Added Entity Layer
    - Added Lombok
    - Added SpaceshipEntity
    - Added Hibernate conf for DDL create
    - Added data.sql for inserts in table for DB
    - Added dependences in pom.xml

---
+++ src/main/java/com/sandav/pruebatecnica/SpaceshipEntity.java
@@ -0,0 +1,17 @@
+package com.sandav.pruebatecnica;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class SpaceshipEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Long speed; // Max speed. Unit: km/h
+}
