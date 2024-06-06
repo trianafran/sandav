3633d7a99764f331ba4764d0d930f9ea5b4c837d
Fran Díaz <trianafran@gmail.com>
2024-06-06 14:11:54 +0200

    OpenAPI
    - Added OpenAPI to proyect
    - Added Swagger UI for Spaceship API

--- src/main/java/com/sandav/pruebatecnica/api/SpaceshipController.java
+++ src/main/java/com/sandav/pruebatecnica/api/SpaceshipController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,44 +19,53 @@ import com.sandav.pruebatecnica.service.SpaceshipService;
 import com.sandav.pruebatecnica.util.mappings.SpaceshipMapper;
 import com.sandav.pruebatecnica.valueObject.Spaceship;
 
-@RestController("/api/v1")
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/v1/spaceships")
+@Tag(name = "Spaceship API")
 public class SpaceshipController {
 	@Autowired
 	SpaceshipService spaceshipService;
 	@Autowired
 	SpaceshipMapper spaceshipMapper;
 
-	@GetMapping("/spaceships")
+	@Operation(summary = "Get a pageabled list of spaceships")
+	@GetMapping("/")
 	List<SpaceshipDto> findAll(@RequestParam Integer page,@RequestParam Integer size) {
 		return mapSpaceshipListToSpaceshipDtoList(spaceshipService.findAll(page, size));
 	}
 	
-	@GetMapping("/spaceships/name/{name}")
+	@Operation(summary = "Get a list of spaceships containing the name value")
+	@GetMapping("/name/{name}")
 	List<SpaceshipDto> findByNameContains(@PathVariable String name) {
 		return mapSpaceshipListToSpaceshipDtoList(spaceshipService.findByNameContains(name));
 	}
 	
-	@PostMapping("/spaceships")
+	@Operation(summary = "Create a spaceship")
+	@PostMapping("/")
 	SpaceshipDto create(@RequestBody SpaceshipDto spaceshipDto) {
 		return spaceshipMapper.spaceshipToSpaceshipDto(spaceshipService.create(spaceshipMapper.spaceshipDtoToSpaceship(spaceshipDto)));
 	}
 	
-	@PutMapping("/spaceships")
+	@Operation(summary = "Upgrade a spaceship entirely")
+	@PutMapping("/")
 	SpaceshipDto update(@RequestBody SpaceshipDto spaceshipDto) {
 		return spaceshipMapper.spaceshipToSpaceshipDto(spaceshipService.update(spaceshipMapper.spaceshipDtoToSpaceship(spaceshipDto)));
 	}
 	
-	@DeleteMapping("/spaceships")
+	@Operation(summary = "Delete a spaceship by id")
+	@DeleteMapping("/")
 	void delete(Long id) {
 		spaceshipService.delete(id);
 	}
 	
-	@GetMapping("/spaceships/id/{id}")
+	@Operation(summary = "Get a spaceship by id")
+	@GetMapping("/id/{id}")
 	SpaceshipDto findById(@PathVariable Long id) {
 		return spaceshipMapper.spaceshipToSpaceshipDto(spaceshipService.findById(id));
 	}
-	
-	
 	
 	private List<SpaceshipDto> mapSpaceshipListToSpaceshipDtoList(List<Spaceship> spaceshipList){
 		List<SpaceshipDto> spaceshipDtoList = new ArrayList<>();
--- src/main/java/com/sandav/pruebatecnica/dto/SpaceshipDto.java
+++ src/main/java/com/sandav/pruebatecnica/dto/SpaceshipDto.java
@@ -1,10 +1,12 @@
 package com.sandav.pruebatecnica.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SpaceshipDto {
 	private Long id;
+	@Size(min = 0, max = 255)
 	private String name;
 	private Long speed; // Max speed. Unit: km/h
 }
